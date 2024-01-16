d56f7128ba39262cc09f49bcaf1ce761ab564387
woues1 <toni.hirvikallio@gmail.com>
2024-01-16 11:37:47 +0200

    initial commit

--- tunti1.py
+++ tunti1.py
@@ -1,1 +1,20 @@
 print("Hello World")
+
+
+
+eka = -9
+toka = 12_456_123_180
+kolmas = 4.973
+neljäs = -4 + 2j
+
+print(eka)
+print(toka)
+print(kolmas)
+print(neljäs)
+print(neljäs.real)
+print(neljäs.imag)
+
+fahrenheit_str = input("Anna lämpötila Fahrenheit-asteina: ")
+fahrenheit = float(fahrenheit_str)
+celsius = (fahrenheit-32)*5/9
+print("Lämpötila Celsius-asteina: " + str(celsius))
