52b69a7145a9de976385ce92eb3a51c9fdba1426
woues1 <toni.hirvikallio@gmail.com>
2024-01-27 11:35:06 +0200

    Functio tehtävä 4-6

--- 6. Function_6.py
+++ 6. Function_6.py
@@ -1,8 +1,4 @@
-'''Kirjoita funktio, joka saa parametreinaan pyöreän pizzan halkaisijan senttimetreinä sekä pizzan hinnan euroina.
-Funktio laskee ja palauttaa pizzan yksikköhinnan euroina per neliömetri.
-Pääohjelma kysyy käyttäjältä kahden pizzan halkaisijat ja hinnat sekä ilmoittaa, kumpi pizza antaa paremman vastineen
-rahalle (eli kummalla on alhaisempi yksikköhinta).
-Yksikköhintojen laskennassa on hyödynnettävä kirjoitettua funktiota.'''
+
 import math
 def pizza_neliömetri_hinta(halkaisija, hinta):
     neliömetri_hinta = hinta/(math.pi * halkaisija)
