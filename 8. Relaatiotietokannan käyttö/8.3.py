6417b6143a4105aa92c389cd11289830fe6870c2
tonih <toni.hirvikallio@gmail.com>
2024-02-13 09:55:41 +0200

    8 tehtävät päivitys

--- 8. Relaatiotietokannan käyttö/8.3.py
+++ 8. Relaatiotietokannan käyttö/8.3.py
@@ -3,12 +3,8 @@ from geopy import distance
 
 
 def etaisyys(icao1, icao2):
-    lentokentta1 = sql_search(f"SELECT latitude_deg, longitude_deg FROM airport WHERE ident = '{icao1}'")
-    lentokentta2 = sql_search(f"SELECT latitude_deg, longitude_deg FROM airport WHERE ident = '{icao2}'")
-
-    print(lentokentta1)
-    print(lentokentta2)
-    laskettu_etaisyys = distance.distance(lentokentta1, lentokentta2).km
+    lentokentat = sql_search(f"SELECT latitude_deg, longitude_deg FROM airport WHERE ident = '{icao1}' OR ident = '{icao2}'")
+    laskettu_etaisyys = distance.distance(*lentokentat).km
     return laskettu_etaisyys
 
 
